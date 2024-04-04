835870c6abfbb4c06d8168a5b24163a05cd1a947
Carlos <krl512109@gmail.com>
2024-04-04 18:23:57 -0400

    ejemplo de app con sql injection y sin sql injection

---
+++ nosecure app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import pymssql
+
+app = Flask(__name__)
+
+def connect_to_sql_server():
+    conn = pymssql.connect(
+        server='ec2-3-92-194-19.compute-1.amazonaws.com',
+        database='AdventureWorks2016',
+        user='sa',
+        password='SqlServer2024**'
+    )
+    return conn
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        # Conectar a la base de datos
+        conn = connect_to_sql_server()
+        cursor = conn.cursor(as_dict=True)
+        cursor.execute(f"SELECT firstname, MiddleName, lastname  FROM Person.Person WHERE firstname = \'{nombre}\'")
+  
+ 
+        users = cursor.fetchall()
+        conn.close()
+        return render_template('resultado.html', nombre=users)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
